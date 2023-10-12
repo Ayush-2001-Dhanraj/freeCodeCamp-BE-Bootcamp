37460a5fe9dc3547a31f0f7ca4fdb912aa147dec
Ayush-2001-Dhanraj <dhanrajaayush123@gmail.com>
2023-10-12 23:42:30 +0530

    Setup controller

--- task-manager/controller/tasks.js
+++ task-manager/controller/tasks.js
@@ -1,17 +1,67 @@
-const getTasks = (req, res) => {
-  res.send("Get all Tasks");
+const Task = require("../models/task");
+
+const getTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find();
+    return res.status(200).json({ tasks });
+  } catch (error) {
+    return res.status(500).json(error);
+  }
 };
-const createTask = (req, res) => {
-  res.json(req.body);
+const createTask = async (req, res) => {
+  try {
+    const newTask = await Task.create(req.body);
+    return res.status(201).json({ newTask });
+  } catch (error) {
+    return res.status(500).json(error);
+  }
 };
-const getTask = (req, res) => {
-  res.json({ id: req.params.id });
+
+const getTask = async (req, res) => {
+  try {
+    const task = await Task.findOne({ _id: req.params.id });
+
+    if (!task) {
+      return res
+        .status(404)
+        .json({ msg: `No task with id ${req.params.id} found` });
+    }
+
+    return res.status(200).json({ task });
+  } catch (error) {
+    return res.status(500).json(error);
+  }
 };
-const updateTask = (req, res) => {
-  res.json({ id: req.params.id, body: req.body });
+const updateTask = async (req, res) => {
+  try {
+    const task = await Task.findOneAndUpdate(
+      { _id: req.params.id },
+      { ...req.body }
+    );
+
+    if (!task) {
+      return res
+        .status(404)
+        .json({ msg: `No task with id ${req.params.id} found` });
+    }
+
+    return res.status(200).json({ task });
+  } catch (error) {
+    return res.status(500).json(error);
+  }
 };
-const deleteTask = (req, res) => {
-  res.json({ id: req.params.id, success: true });
+const deleteTask = async (req, res) => {
+  try {
+    const task = await Task.findOneAndDelete({ _id: req.params.id });
+
+    if (!task)
+      return res
+        .status(404)
+        .json({ msg: `No task with id ${req.params.id} found` });
+    return res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json(error);
+  }
 };
 
 module.exports = { getTasks, createTask, getTask, updateTask, deleteTask };
