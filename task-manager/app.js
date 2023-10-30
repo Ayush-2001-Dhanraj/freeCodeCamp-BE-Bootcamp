961376ce169edffa995907f0e97ef6f415bc40fc
Ayush-2001-Dhanraj <dhanrajaayush123@gmail.com>
2023-10-30 11:22:38 +0530

    Port number from env

--- task-manager/app.js
+++ task-manager/app.js
@@ -6,7 +6,7 @@ const errorHandler = require("./middleware/errorHandler");
 require("dotenv").config();
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.static("./public"));
 app.use("/api/v1/tasks", tasksRoutes);
