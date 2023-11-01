7cc7d7e32ee9867642f884772c0ae57c9b5eafe2
Ayush-2001-Dhanraj <dhanrajaayush123@gmail.com>
2023-11-01 19:34:36 +0530

    login, register, jwt, bcrypt

--- jobs-api/app.js
+++ jobs-api/app.js
@@ -4,6 +4,9 @@ const express = require("express");
 const app = express();
 
 // error handler
+const connectDB = require("./db/connect");
+const authRoutes = require("./routes/auth");
+const jobsRoutes = require("./routes/jobs");
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
@@ -11,9 +14,8 @@ app.use(express.json());
 // extra packages
 
 // routes
-app.get("/", (req, res) => {
-  res.send("jobs api");
-});
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/jobs", jobsRoutes);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
@@ -22,6 +24,7 @@ const port = process.env.PORT || 5000;
 
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
