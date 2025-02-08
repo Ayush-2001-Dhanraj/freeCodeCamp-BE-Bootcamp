71eb6858e6a88e1d1d6e61fb04c2c4978d4a305a
Ayush-2001-Dhanraj <dhanrajaayush123@gmail.com>
2025-02-08 14:45:38 +0530

    google oauth + add/update secret

--- 9.1 AUthentication lev 1/index.js
+++ 9.1 AUthentication lev 1/index.js
@@ -4,18 +4,21 @@ import pg from "pg";
 import bcrypt from "bcrypt";
 import passport from "passport";
 import { Strategy } from "passport-local";
+import GoogleStrategy from "passport-google-oauth2";
 import session from "express-session";
+import env from "dotenv";
 
 const app = express();
 const port = 3000;
 const saltRounds = 10;
+env.config();
 
 const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "secrets",
-  password: "123456",
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 
 db.connect();
@@ -27,7 +30,7 @@ app.use(
   session({
     resave: false,
     saveUninitialized: true,
-    secret: "TOPSECRET",
+    secret: process.env.SESSION_SECRET,
     cookie: {
       maxAge: 1000 * 60 * 60 * 24,
     },
@@ -48,13 +51,72 @@ app.get("/register", (req, res) => {
   res.render("register.ejs");
 });
 
-app.get("/secrets", (req, res) => {
+app.get("/submit", (req, res) => {
   if (req.isAuthenticated()) {
-    res.render("secrets.ejs");
+    res.render("submit.ejs");
   } else {
     res.redirect("/login");
   }
 });
+
+app.post("/submit", async (req, res) => {
+  const { secret } = req.body;
+  const { email } = req.user;
+
+  const response = await db.query(
+    "UPDATE users SET secret = $1 WHERE email = $2 RETURNING *",
+    [secret, email]
+  );
+
+  if (response.rows.length) res.redirect("/secrets");
+});
+
+app.get("/secrets", async (req, res) => {
+  if (req.isAuthenticated()) {
+    const { email } = req.user;
+    const response = await db.query(
+      "SELECT secret from users WHERE email = $1",
+      [email]
+    );
+    res.render("secrets.ejs", {
+      secret: response.rows[0].secret
+        ? response.rows[0].secret
+        : "something...",
+    });
+  } else {
+    res.redirect("/login");
+  }
+});
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  })
+);
+
+app.get(
+  "/auth/google/secrets",
+  passport.authenticate("google", {
+    successRedirect: "/secrets",
+    failureRedirect: "/login",
+  })
+);
+
+app.get("/logout", (req, res) => {
+  req.logout((err) => {
+    if (err) console.log(err);
+    res.redirect("/");
+  });
+});
+
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    failureRedirect: "/login",
+    successRedirect: "/secrets",
+  })
+);
 
 app.post("/register", async (req, res) => {
   const { username, password } = req.body;
@@ -88,15 +150,8 @@ app.post("/register", async (req, res) => {
   }
 });
 
-app.post(
-  "/login",
-  passport.authenticate("local", {
-    failureRedirect: "/login",
-    successRedirect: "/secrets",
-  })
-);
-
 passport.use(
+  "local",
   new Strategy(async function verify(username, password, cb) {
     try {
       const retrievedUser = await db.query(
@@ -127,6 +182,38 @@ passport.use(
       return cb(e);
     }
   })
+);
+
+passport.use(
+  "google",
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "http://localhost:3000/auth/google/secrets",
+      userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
+    },
+    async (accessToken, refreshToken, profile, cb) => {
+      try {
+        const retrievedUser = await db.query(
+          "SELECT * from users where email = $1",
+          [profile.email]
+        );
+
+        if (retrievedUser.rows.length) {
+          cb(null, retrievedUser.rows[0]);
+        } else {
+          const newUser = await db.query(
+            "INSERT INTO users (email, password) VALUES ($1, $2) RETURNING *",
+            [profile.email, "google"]
+          );
+          cb(null, newUser.rows[0]);
+        }
+      } catch (e) {
+        return cb(e);
+      }
+    }
+  )
 );
 
 passport.serializeUser((user, cb) => {
